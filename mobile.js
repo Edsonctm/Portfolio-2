7e0184f8101d2d8419812aa08a52891401de4da4
Edsonctm <Edson_ctm@hotmail.com>
2022-04-24 16:37:43 -0300

    Initial commit
    .

---
+++ mobile.js
@@ -0,0 +1,9 @@
+/*Função para o menu sanduíche// Function for sandwich menu*/
+var menuBar = document.querySelector(".menuBarPrinc")
+
+function myFunction(x) {
+    x.classList.toggle("change");
+    menuBar.classList.toggle("menuOn")
+  }
+
+/*Fim da função para o menu sanduíche// End of function for sandwich menu*/
